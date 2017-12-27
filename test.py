8072720ce75e161adf98182b3ae40efe42f231a7
chandong83 <chandong83@naver.com>
2017-12-27 15:20:22 +0900

    first

---
+++ test.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+import OttoBuzzer as OB
+from time import sleep
+pinBuzzer = 17  # pin Number 7
+
+ottoTone = OB.Tone(pinBuzzer)
+ottoTone.start()
+
+#playSounds만큼 루프
+for playSound in OB.playSounds:
+    print(playSound)
+    #플레이
+    ottoTone.sing(playSound)
+
+    sleep(1)
